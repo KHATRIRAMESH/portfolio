a73a7a0a792096aecdbddc2ca4adc2d849345bf5
KHATRIRAMESH <khatriramesh972@gmail.com>
2025-12-25 10:11:17 +0545

    minor change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,22 @@
+import { icons } from "lucide-react";
 import "./globals.css";
 
 export const metadata = {
-    title: 'Developers',
-    description: 'Building the future of web development',
+  title: "Developers",
+  description: "Building the future of web development",
+  icons: { icon: "/terminal_favicon.ico" },
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <head>
-                <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-            </head>
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,19 @@
-'use client';
+"use client";
 
-import Acomplishments from '@/components/Acomplishments/Acomplishments';
-import Hero from '@/components/Hero/Hero';
-import Projects from '@/components/Projects/Projects';
-import Technologies from '@/components/Technologies/Technologies';
-import Timeline from '@/components/TimeLine/TimeLine';
-import Blogs from '@/components/Blogs/Blogs';
-import { Layout } from '@/layout/Layout';
+import Hero from "@/components/Hero/Hero";
+import Projects from "@/components/Projects/Projects";
+import Technologies from "@/components/Technologies/Technologies";
+import Timeline from "@/components/TimeLine/TimeLine";
+import { Layout } from "@/layout/Layout";
 const Home = () => {
-    return (
-        <Layout>
-            <Hero />
-            <Projects />
-            <Technologies />
-            <Timeline />
-            {/* <Blogs /> */}
-            {/* <Acomplishments /> */}
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Hero />
+      <Projects />
+      <Technologies />
+      <Timeline />
+    </Layout>
+  );
 };
 
 export default Home;
